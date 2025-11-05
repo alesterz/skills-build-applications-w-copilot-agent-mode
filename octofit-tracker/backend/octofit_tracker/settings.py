64400c3867c9ec9972b1bbbad139eea214e6986e
alesterz <alessandro.sterza@accenture.com>
2025-11-05 10:06:15 +0000

    third commit

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-=!xaly16x^m9gk15i!z#no=p*mx-xe5x5mm$@i_$xpvzchh@bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+
+# Ottieni il nome del codespace dalla variabile d'ambiente
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 
 # Application definition
